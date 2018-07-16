30bacd7961f5275bafe3294119dc15705425e23c
alex <zhurbitsky@gmail.com>
2018-07-16 10:49:14 +0300

    Button test

--- app/src/main/java/com/example/alex/androidthingsperipherals/HomeActivity.java
+++ app/src/main/java/com/example/alex/androidthingsperipherals/HomeActivity.java
@@ -4,7 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
 import com.google.android.things.pio.PeripheralManager;
+
+import java.io.IOException;
 
 /**
  * Skeleton of an Android Things activity.
@@ -27,6 +31,10 @@ import com.google.android.things.pio.PeripheralManager;
  */
 public class HomeActivity extends Activity {
     private static final String TAG = "HomeActivity";
+    private static final String BUTTON_PIN_NAME = "GPIO6_IO14";
+
+    // GPIO connection to button input
+    private Gpio mButtonGpio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +42,49 @@ public class HomeActivity extends Activity {
 
         PeripheralManager pioManager = PeripheralManager.getInstance();
         Log.d(TAG, "Available GPIO: " + pioManager.getGpioList());
+
+        try {
+            // Create GPIO connection.
+            mButtonGpio = pioManager.openGpio(BUTTON_PIN_NAME);
+
+            // Configure as an input, trigger events on every change.
+            mButtonGpio.setDirection(Gpio.DIRECTION_IN);
+            mButtonGpio.setEdgeTriggerType(Gpio.EDGE_BOTH);
+            // Value is true when the pin is LOW
+            mButtonGpio.setActiveType(Gpio.ACTIVE_LOW);
+            // Register the event callback.
+            mButtonGpio.registerGpioCallback(mCallback);
+        } catch (IOException e) {
+            Log.w(TAG, "Error opening GPIO", e);
+        }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Close the button
+        if (mButtonGpio != null) {
+            mButtonGpio.unregisterGpioCallback(mCallback);
+            try {
+                mButtonGpio.close();
+            } catch (IOException e) {
+                Log.w(TAG, "Error closing GPIO", e);
+            }
+        }
+    }
+
+    private GpioCallback mCallback = new GpioCallback() {
+        @Override
+        public boolean onGpioEdge(Gpio gpio) {
+            try {
+                Log.i(TAG, "GPIO changed, button " + gpio.getValue());
+            } catch (IOException e) {
+                Log.w(TAG, "Error reading GPIO");
+            }
+
+            // Return true to keep callback active.
+            return true;
+        }
+    };
 }
